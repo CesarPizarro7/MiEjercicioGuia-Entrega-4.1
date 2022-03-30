334183f6b725ec68bfda67afcca45eab000303cd
César Pizarro <cesar@CpTO-Laptop.local>
2022-03-30 22:01:16 +0200

    VersionConConexionDesconexion

--- clienteEjercicioGuia/WindowsFormsApplication1/Form1.Designer.cs
+++ clienteEjercicioGuia/WindowsFormsApplication1/Form1.Designer.cs
@@ -32,11 +32,13 @@
             this.nombre = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Alto = new System.Windows.Forms.RadioButton();
+            this.textBoxAltura = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.Longitud = new System.Windows.Forms.RadioButton();
             this.Bonito = new System.Windows.Forms.RadioButton();
-            this.textBoxAltura = new System.Windows.Forms.TextBox();
-            this.Alto = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Desconectar = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -81,7 +83,7 @@
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.button2);
             this.groupBox1.Controls.Add(this.nombre);
-            this.groupBox1.Location = new System.Drawing.Point(16, 140);
+            this.groupBox1.Location = new System.Drawing.Point(16, 80);
             this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
@@ -89,6 +91,24 @@
             this.groupBox1.TabIndex = 6;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Peticion";
+            // 
+            // Alto
+            // 
+            this.Alto.AutoSize = true;
+            this.Alto.Location = new System.Drawing.Point(155, 143);
+            this.Alto.Name = "Alto";
+            this.Alto.Size = new System.Drawing.Size(94, 21);
+            this.Alto.TabIndex = 10;
+            this.Alto.TabStop = true;
+            this.Alto.Text = "Soy alt@?";
+            this.Alto.UseVisualStyleBackColor = true;
+            // 
+            // textBoxAltura
+            // 
+            this.textBoxAltura.Location = new System.Drawing.Point(78, 142);
+            this.textBoxAltura.Name = "textBoxAltura";
+            this.textBoxAltura.Size = new System.Drawing.Size(57, 22);
+            this.textBoxAltura.TabIndex = 9;
             // 
             // label1
             // 
@@ -125,29 +145,35 @@
             this.Bonito.Text = "Dime si mi nombre es bonito";
             this.Bonito.UseVisualStyleBackColor = true;
             // 
-            // textBoxAltura
+            // button1
             // 
-            this.textBoxAltura.Location = new System.Drawing.Point(78, 142);
-            this.textBoxAltura.Name = "textBoxAltura";
-            this.textBoxAltura.Size = new System.Drawing.Size(57, 22);
-            this.textBoxAltura.TabIndex = 9;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 61);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Conectar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Conectar_Click);
             // 
-            // Alto
+            // Desconectar
             // 
-            this.Alto.AutoSize = true;
-            this.Alto.Location = new System.Drawing.Point(155, 143);
-            this.Alto.Name = "Alto";
-            this.Alto.Size = new System.Drawing.Size(94, 21);
-            this.Alto.TabIndex = 10;
-            this.Alto.TabStop = true;
-            this.Alto.Text = "Soy alt@?";
-            this.Alto.UseVisualStyleBackColor = true;
+            this.Desconectar.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Desconectar.Location = new System.Drawing.Point(297, 12);
+            this.Desconectar.Name = "Desconectar";
+            this.Desconectar.Size = new System.Drawing.Size(203, 61);
+            this.Desconectar.TabIndex = 8;
+            this.Desconectar.Text = "Desconectar";
+            this.Desconectar.UseVisualStyleBackColor = true;
+            this.Desconectar.Click += new System.EventHandler(this.Desconectar_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(988, 692);
+            this.ClientSize = new System.Drawing.Size(515, 441);
+            this.Controls.Add(this.Desconectar);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox1);
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
@@ -170,6 +196,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.RadioButton Alto;
         private System.Windows.Forms.TextBox textBoxAltura;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Desconectar;
     }
 }
 
